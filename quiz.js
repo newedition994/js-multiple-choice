d93bf814eeeecb0b62fd6522726ed08857193902
Breyanna Vass <breyannavass@gmail.com>
2019-10-29 15:06:56 -0400

    selected all the elements

---
+++ quiz.js
@@ -0,0 +1,12 @@
+// Select all the elements
+const start = document.getElementById("start");
+const quiz = document.getElementById("quiz");
+const question = document.getElementById("question");
+const qImg = document.getElementById("qImg");
+const choiceA = document.getElementById("A");
+const choiceB = document.getElementById("B");
+const choiceC = document.getElementById("C");
+const counter = document.getElementById("counter");
+const timeGauge = document.getElementById("timeGauge");
+const progress = document.getElementById("progress");
+const scoreDiv = document.getElementById("scoreContainer");
